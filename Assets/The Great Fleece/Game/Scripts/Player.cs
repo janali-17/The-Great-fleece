fc8172d388f88348735376d28fa35663e138438c
Jan Ali <68240754+janali-17@users.noreply.github.com>
2024-07-14 04:19:59 -0700

    Added NavMeshAgent and player movement

---
+++ Assets/The Great Fleece/Game/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour
+{
+    //Variables
+
+    //Prefabs && Handles
+     private NavMeshAgent _navMeshAgent;
+
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+          
+            Ray rayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if(Physics.Raycast(rayOrigin, out hitInfo))
+            {
+                Debug.Log("Hit : " + hitInfo.point);
+                _navMeshAgent.transform.position = hitInfo.point;
+
+               // GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+               // cube.transform.position = hitInfo.point;
+            }
+
+        }
+        
+    }
+}
