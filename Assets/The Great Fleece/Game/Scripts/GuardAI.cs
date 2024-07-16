f961d4e668cbc9fad06edc3aa6e030efce2d8f6c
Jan Ali <68240754+janali-17@users.noreply.github.com>
2024-07-16 08:04:48 -0700

    Ai WayPoints SetUp

--- Assets/The Great Fleece/Game/Scripts/GuardAI.cs
+++ Assets/The Great Fleece/Game/Scripts/GuardAI.cs
@@ -1,18 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GuardAI : MonoBehaviour
 {
+    // Variables
     public List<Transform> wayPoints;
+    public Transform CurrentTarget;
+
+    // Prefabs
+    NavMeshAgent _navMeshAgent;
 
     void Start()
     {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
         
+        if(wayPoints.Count > 0)
+        {
+            if(wayPoints[0] != null)
+            {
+                CurrentTarget = wayPoints[0];
+            }
+        }
     }
 
     void Update()
     {
+        _navMeshAgent.destination = CurrentTarget.position;
         
     }
 }
