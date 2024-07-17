86f3182a111f1a9bab4c20ce3d51afcc1571cf1e
Jan Ali <68240754+janali-17@users.noreply.github.com>
2024-07-17 11:46:31 -0700

    GuardAi move forward and reverse

--- Assets/The Great Fleece/Game/Scripts/GuardAI.cs
+++ Assets/The Great Fleece/Game/Scripts/GuardAI.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -8,7 +9,9 @@ public class GuardAI : MonoBehaviour
     // Variables
     public List<Transform> wayPoints;
     [SerializeField]
-    private int  _CurrentTarget;
+    private int _CurrentTarget;
+    private bool _reverse = false;
+    private bool _targetReached = false;
 
 
     // Prefabs
@@ -17,8 +20,8 @@ public class GuardAI : MonoBehaviour
     void Start()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
-        
-        
+
+
     }
 
     void Update()
@@ -28,11 +31,50 @@ public class GuardAI : MonoBehaviour
             _navMeshAgent.SetDestination(wayPoints[_CurrentTarget].position);
         }
         float distance = Vector3.Distance(transform.position, wayPoints[_CurrentTarget].position);
-        if(distance < 1.0f)
+        if (distance < 1.0f && _targetReached == false)
+        {
+            _targetReached = true;
+            Debug.Log("Target Has Been Reached " + _targetReached);
+
+            StartCoroutine(WaitForNextPoint());
+        }
+    }
+
+    IEnumerator WaitForNextPoint()
+    {
+        Debug.Log("wait for 5 seconds");
+        float randRang = UnityEngine.Random.Range(5.0f, 7.0f);
+        if(_CurrentTarget == 0)
+        {
+            yield return new WaitForSeconds(randRang);
+        }
+        else if(_CurrentTarget == wayPoints.Count - 1)
+        {
+            yield return new WaitForSeconds(randRang);
+        }
+
+        
+        if (_reverse == true)
+        {
+            _CurrentTarget--;
+            if (_CurrentTarget == 0)
+            {
+                _reverse = false;
+                _CurrentTarget = 0;
+            }
+        }
+        else
         {
             _CurrentTarget++;
         }
 
-        
+        if (_CurrentTarget == wayPoints.Count)
+        {
+            _reverse = true;
+            _CurrentTarget--;
+        }
+
+        _targetReached = false;
     }
+
 }
