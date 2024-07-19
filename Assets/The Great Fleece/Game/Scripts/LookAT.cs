4d14963e14d969cf19ecdce2dcc98b081f48c00d
Jan Ali <68240754+janali-17@users.noreply.github.com>
2024-07-19 01:21:26 -0700

    Main Camera setup

--- Assets/The Great Fleece/Game/Scripts/LookAT.cs
+++ Assets/The Great Fleece/Game/Scripts/LookAT.cs
@@ -5,6 +5,13 @@ using UnityEngine;
 public class LookAT : MonoBehaviour
 {
     public Transform lookAt;
+    public Transform StartCamera;
+
+    private void Start()
+    {
+        transform.position = StartCamera.transform.position;
+        transform.rotation = StartCamera.transform.rotation;
+    }
     void Update()
     {
         transform.LookAt(lookAt);
