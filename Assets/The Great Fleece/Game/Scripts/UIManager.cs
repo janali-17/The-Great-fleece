29ab124e95b4966077a6d3dfbec9e4009d636a3c
Jan Ali <68240754+janali-17@users.noreply.github.com>
2024-07-24 10:12:45 -0700

    added Ui manager script and Restart and quit function

---
+++ Assets/The Great Fleece/Game/Scripts/UIManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    private static UIManager _instance;
+    
+    public static UIManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                Debug.LogError("The UI manager instance is Empty");
+            }
+            return _instance;
+        }
+    }
+    private void Awake()
+    {
+        _instance = this;
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    
+}
