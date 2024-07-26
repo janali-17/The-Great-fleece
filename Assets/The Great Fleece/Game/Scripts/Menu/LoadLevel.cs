4ab42b11a8f60ed127809f09a597c91d1235b43c
Jan Ali <68240754+janali-17@users.noreply.github.com>
2024-07-26 12:33:12 -0700

    Git issue resolved and gaming is running fine with loadscene

---
+++ Assets/The Great Fleece/Game/Scripts/Menu/LoadLevel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadLevel : MonoBehaviour
+{
+    [SerializeField]
+    private Image Progessbar;
+    void Start()
+    {
+        StartCoroutine(LoadSceneAsync());
+    }
+
+    IEnumerator LoadSceneAsync()
+    {
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(2);
+
+        while (asyncOperation.isDone == false)
+        {
+            Progessbar.fillAmount = asyncOperation.progress;
+            yield return new WaitForEndOfFrame();
+        }
+    }
+}
